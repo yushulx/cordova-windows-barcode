3701b4899f654031afc0bb7415b5c3950a684aee
Xiao Ling <lingxiao1002@gmail.com>
2018-11-20 15:26:17 +0800

    Removed hook script

--- hooks/windows/copy-dlls.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-var dllSrc = path.join(process.cwd(), 'plugins', 'barcode', 'src', 'windows', 'libs', 'x86', 'Dynamsoft.dll');
-var dllDest = path.join(process.cwd(), 'platforms', 'windows', 'plugins', 'barcode', 'x86', 'Dynamsoft.dll');
-fs.createReadStream(dllSrc).pipe(fs.createWriteStream(dllDest));
-
